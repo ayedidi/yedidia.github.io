d37f922c1a5b66aee22af063112ceb0eed2c0420
elladia <68752179+elladia@users.noreply.github.com>
2020-09-19 18:16:17 -0500

    caption

--- js/slide.js
+++ js/slide.js
@@ -50,3 +50,75 @@ window.addEventListener('scroll', function() {
     });
     
 
+
+    new hoverEffect({
+      parent: document.querySelector('.distortion'),
+      intensity: 0.2,
+      image1: '/img/secYoga.png',
+      image2: '/img/repeat.png',
+      displacementImage: '/img/diss2.png',
+      imagesRatio: 380 / 300
+    })
+
+    // MEDIA
+    TweenMax.staggerFrom(".media ul li", 1.5, {
+      delay: 1.5,
+      opacity: 0,
+      x: "-20",
+      ease: Expo.easeInOut
+    }, 0.08);
+
+    // TEXT
+    TweenMax.from(".text h1 .hidetext", 1.5, {
+      delay: 1,
+      y: "100%",
+      ease: Expo.easeInOut
+    });
+
+    TweenMax.from(".text h3 .hidetext", 1.5, {
+      delay: 1.2,
+      y: "100%",
+      ease: Expo.easeInOut
+    });
+
+    TweenMax.from(".text p .hidetext", 1.5, {
+      delay: 1.3,
+      y: "100%",
+      ease: Expo.easeInOut
+    });
+
+    TweenMax.from(".text h2", 1.5, {
+      delay: 1.5,
+      opacity: 0,
+      x: "-10000",
+      ease: Expo.easeInOut
+    });
+
+
+    // DISTORTION
+    TweenMax.from(".distortion", 1.5, {
+      delay: 2,
+      opacity: 0,
+      y: "20",
+      ease: Expo.easeInOut
+    });
+
+    // OVERLAY
+    TweenMax.to(".first", 1.5, {
+      delay: .5,
+      top: "-100%",
+      ease: Expo.easeInOut
+    });
+
+    TweenMax.to(".second", 1.5, {
+      delay: .7,
+      top: "-100%",
+      ease: Expo.easeInOut
+    });
+
+    TweenMax.to(".third", 1.5, {
+      delay: .9,
+      top: "-100%",
+      ease: Expo.easeInOut
+    });
+
