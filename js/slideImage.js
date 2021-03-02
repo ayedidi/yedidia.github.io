df202fc98227559fa52d0d4a6b7dc4b1a090b9a2
elladia <68752179+elladia@users.noreply.github.com>
2021-03-02 13:40:53 -0600

    yeeeeesh

--- js/slideImage.js
+++ js/slideImage.js
@@ -1,33 +1,3 @@
-//var slideIndex = 1;
-//showSlides(slideIndex);
-//
-//// Next/previous controls
-//function plusSlides(n) {
-//  showSlides(slideIndex += n);
-//}
-//
-//// Thumbnail image controls
-//function currentSlide(n) {
-//  showSlides(slideIndex = n);
-//}
-//
-//function showSlides(n) {
-//  var i;
-//  var slides = document.getElementsByClassName("mySlides");
-//  var dots = document.getElementsByClassName("dot");
-//  if (n > slides.length) {slideIndex = 1}
-//  if (n < 1) {slideIndex = slides.length}
-//  for (i = 0; i < slides.length; i++) {
-//      slides[i].style.display = "none";
-//  }
-// 
-//  slides[slideIndex-1].style.display = "block";
-//  
-//}
-//
-//showSlides();
-
-
 function initComparisons() {
   var x, i;
   /* Find all elements with an "overlay" class: */
@@ -104,3 +74,4 @@ function initComparisons() {
     }
   }
 }
+initComparisons();
