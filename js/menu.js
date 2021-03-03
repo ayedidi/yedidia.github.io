6433dab81f7a5bf69a95583d5731c111bdddaa77
elladia <68752179+elladia@users.noreply.github.com>
2021-03-02 20:32:04 -0600

    menu!

--- js/menu.js
+++ js/menu.js
@@ -1,25 +1,51 @@
-const menuBtn = document.getElementById('#menu_button');
-let menuOpen = false;
+(function(){
+  'use strict';
 
-
-menuBtn.addEventListener('click', () => {
-    if(!menuOpen) {
-        openNav();
-    } else {
-        closeNav();
+  class Menu {
+    constructor(settings) {
+      this.menuNode = settings.menuNode;
+      this.state = false;
+      this.menuStateTextNode = settings.menuStateTextNode || this.menuNode.querySelector('.menu__screen-reader');
+      this.menuOpenedText = settings.menuOpenedText || 'Open menu';
+      this.menuClosedText = settings.menuClosedText || 'Close menu';
     }
-});
+    
+    changeState(state) {
+      return this.state = !state;
+    }
+    
+    changeStateText(state, node) {
+      let text = (state !== true) ? this.menuOpenedText : this.menuClosedText;
+      
+      node.textContent = text;
+      return text; 
+    }
+           
+    toggleMenuState(className) {   
+      
+      let state;
+      
+      if (typeof className !== 'string' || className.length === 0) {
+        return console.log('you did not give the class for the toggleState function');
+      } 
+            
+      state = this.changeState(this.state);
+      
+      this.changeStateText(state, this.menuStateTextNode);
+      this.menuNode.classList.toggle(className);
+      
+      return state;  
+    }
+  }
 
-function openNav() {
-  document.getElementById("myNav").style.width = "100%";
-    menuBtn.classList.add('open');
-    menuOpen = true;
-}
-
-function closeNav() {
-  document.getElementById("myNav").style.width = "0%";
-    menuBtn.classList.remove('open');
-    menuOpen = false;
-}
-
-
+  const jsMenuNode = document.querySelector('.menu');
+  const demoMenu = new Menu ({
+    menuNode: jsMenuNode
+  });
+  
+  function callMenuToggle(event) {
+    demoMenu.toggleMenuState('menu_activated');
+  }
+  
+  jsMenuNode.querySelector('.menu__toggle').addEventListener('click', callMenuToggle);
+})();
